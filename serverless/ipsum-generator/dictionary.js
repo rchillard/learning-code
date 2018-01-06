6de9ac267830db4ede423821b6a9aa9907eb0763
Ryan Hillard <ryan.hillard@sba.gov>
2018-01-06 18:25:31 -0500

    Adding serverless folder

---
+++ serverless/ipsum-generator/dictionary.js
@@ -0,0 +1,14 @@
+module.exports = [
+  'fire-attack',
+  'zero-men',
+  'pay-per-fireball',
+  'villagedeath',
+  'fly-away',
+  'raid',
+  'wings',
+  'cloud',
+  'NoMen',
+  'Wingda',
+  'drakeservices',
+  'eating-villagers'
+]
