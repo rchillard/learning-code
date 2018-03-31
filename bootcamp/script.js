276e7f923a73306464379f0025a123057133535a
RCH <rchillard@gmail.com>
2018-03-31 17:14:46 -0500

    Basic JS exercise

---
+++ bootcamp/script.js
@@ -0,0 +1,6 @@
+var first_name = prompt("what is your name?");
+var last_name = prompt("what is your surname?");
+var age = prompt("how old are you?");
+
+alert("nice to meet you " + first_name + " " + last_name + ". I am glad you are " + age + " years old.");
+console.log(username);
