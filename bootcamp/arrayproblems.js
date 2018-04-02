2fa8b05bbbb43fcf53d015dbf9e8710a850ce59f
RCH <rchillard@gmail.com>
2018-04-02 08:24:02 -0500

    Fixing issue with isUniform

--- bootcamp/arrayproblems.js
+++ bootcamp/arrayproblems.js
@@ -1,7 +1,7 @@
 console.log('Reverse Function Test')
 
 function reverseArray (arr) {
-  for (var i = arr.length; i >= 0; i--) {
+  for (var i = arr.length - 1; i >= 0; i--) {
     console.log(arr[i])
   }
 }
@@ -11,18 +11,29 @@ var test = ['Cat', 'Dog', 'Parrot', 'Fish']
 reverseArray(test)
 
 console.log('Uniform Function Test')
+// 1st attempt (forEach proves problematic)
+// function isUniform (arr) {
+//   return arr.forEach(function (ar) {
+//     if (arr[0] !== ar) {
+//       console.log(ar)
+//       return false
+//     }
+//     return true
+//   })
+// }
 
+// 2nd attempt (confirmed working)
 function isUniform (arr) {
-  arr.forEach(function (ar) {
-    if (arr[0] !== ar) {
-      console.log(ar)
+  var base = arr[0]
+  for (var i = 1; i < arr.length; i++) {
+    if (base !== arr[i]) {
       return false
     }
-  })
+  }
   return true
 }
 
-var uni = [1, 1, 1, 4, 1, 1]
+var uni = [1, 1, 1, 1, 1, 1, 1, 1, -50]
 
 console.log(isUniform(uni))
 
@@ -33,14 +44,20 @@ function sumArray (arr) {
   arr.forEach(function (ar) {
     sum = sum + ar
   })
+  return sum
 }
 
 console.log(sumArray(uni))
 
-console.log('Sum Function Test')
-
 function max (arr) {
+  var maxAr = 0
   arr.forEach(function (ar) {
-    
+    if (ar > maxAr) {
+      maxAr = ar
+    }
   })
+  return maxAr
 }
+
+console.log('Max Function Test')
+console.log(max(uni))
