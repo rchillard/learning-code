61e25196e0bc2ddb2ef4de8c6dc0ebdfba5ff0f1
RCH <rchillard@gmail.com>
2018-05-28 19:03:28 -0500

    Add Part 2 of the Color Game exercise

--- bootcamp/color-game.js
+++ bootcamp/color-game.js
@@ -1,45 +1,90 @@
 var bannerDisplay = document.getElementsByClassName('banner')[0]
 var messageDisplay = document.getElementById('message')
-var squares = document.querySelectorAll('.square')
-
-var randomColors = generateRandomColors(6)
-var pickedColor = pickColor()
+var squares = document.getElementsByClassName('square')
 var colorDisplay = document.getElementById('colorCode')
-colorDisplay.textContent = pickedColor
+var resetButton = document.querySelector('#reset')
+var easyButton = document.querySelector('#easy')
+var mediumButton = document.querySelector('#medium')
+var hardButton = document.querySelector('#hard')
 
-for (var i = 0; i < squares.length; i++) {
-  // add initial colors to squares
-  squares[i].style.backgroundColor = randomColors[i]
-
-  // add click listeners to squares
-  squares[i].addEventListener('click', function () {
-    // grab the color of the clicked square
-    var clickedColor = this.style.backgroundColor
-    // compare color to pickedColor
-    if (clickedColor === pickedColor) {
-      // notify player they won the game
-      messageDisplay.textContent = 'Correct, well played!'
-      changeColors(clickedColor)
-    } else {
-      // disappear the box
-      messageDisplay.textContent = 'Try again, please!'
-      this.style.opacity = 0.0
-    }
-  })
-}
-
-function changeColors (color) {
-  // loop through all squares
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = color
+resetButton.addEventListener('click', function () {
+  if (easyButton.classList.contains('selected')) {
+    generateBoard(3)
+  } else if (mediumButton.classList.contains('selected')) {
+    generateBoard(6)
+  } else if (hardButton.classList.contains('selected')) {
+    generateBoard(9)
+  } else {
+    window.alert('No valid game state detected!')
   }
-  bannerDisplay.style.backgroundColor = color
-  // change each color to match given color
+  messageDisplay.textContent = 'A new game, good luck!'
+})
+
+easyButton.addEventListener('click', function () {
+  easyButton.classList.add('selected')
+  mediumButton.classList.remove('selected')
+  hardButton.classList.remove('selected')
+  generateBoard(3)
+})
+
+mediumButton.addEventListener('click', function () {
+  mediumButton.classList.add('selected')
+  easyButton.classList.remove('selected')
+  hardButton.classList.remove('selected')
+  generateBoard(6)
+})
+
+hardButton.addEventListener('click', function () {
+  hardButton.classList.add('selected')
+  easyButton.classList.remove('selected')
+  mediumButton.classList.remove('selected')
+  generateBoard(9)
+})
+
+function generateBoard (num) {
+  // reset the Board
+  resetBoard(num)
+  // generate random colors
+  var randomColors = generateRandomColors(num)
+  // pick a winning color (victory condition)
+  var pickedColor = pickRandom(randomColors, num)
+  colorDisplay.textContent = pickedColor
+
+  // apply eventListeners to DOM objects (div elements)
+  for (var i = 0; i < num; i++) {
+    // add initial colors to squares
+    squares[i].style.backgroundColor = randomColors[i]
+    // add click listeners to squares
+    squares[i].addEventListener('click', function () {
+      // grab the color of the clicked square
+      var clickedColor = this.style.backgroundColor
+      // compare color to pickedColor
+      if (clickedColor === pickedColor) {
+        // notify player they won the game
+        messageDisplay.textContent = 'Correct, well played!'
+        changeColors(clickedColor, num)
+        for (var i = 0; i < num; i++) {
+          squares[i].style.opacity = 1.0
+        }
+      } else {
+        // disappear the box
+        messageDisplay.textContent = 'Try again, please!'
+        this.style.opacity = 0.0
+      }
+    })
+  }
 }
 
-function pickColor () {
-  var random = Math.floor(Math.random() * colors.length)
-  return colors[random]
+function resetBoard (num) {
+  // hide the whole board
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.opacity = 0.0
+  }
+
+  // reveal appropriate to the mode
+  for (var j = 0; j < num; j++) {
+    squares[j].style.opacity = 1.0
+  }
 }
 
 function generateRandomColors (num) {
@@ -47,7 +92,7 @@ function generateRandomColors (num) {
   var arr = []
   // fill it with random numbers
   for (var i = 0; i < num; i++) {
-    // get random color and push into array
+    // get random color and assign it to array
     arr[i] = randomColor()
   }
   // return the array
@@ -61,5 +106,22 @@ function randomColor () {
   var greenColor = Math.floor(Math.random() * 256)
   // pick a blue from 0 to 255
   var blueColor = Math.floor(Math.random() * 256)
-  return 'rgb(' + redColor + ' ,' + greenColor + ' ,' + blueColor + ')'
+  return 'rgb(' + redColor + ', ' + greenColor + ', ' + blueColor + ')'
 }
+
+function pickRandom (arr, num) {
+  var random = Math.floor(Math.random() * num)
+  return arr[random]
+}
+
+function changeColors (color, num) {
+  // loop through all num of squares
+  for (var i = 0; i < num; i++) {
+    squares[i].style.backgroundColor = color
+    squares[i].style.opacity = 1.0
+  }
+  bannerDisplay.style.backgroundColor = color
+  // change each color to match given color
+}
+
+generateBoard(6)
