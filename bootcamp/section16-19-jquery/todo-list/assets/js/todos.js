93b517ffc44014d50b7be22e302476ea5c8fb8b1
RCH <rchillard@gmail.com>
2018-07-15 10:02:55 -0500

    Finish up to-do list exercise

--- bootcamp/section16-19-jquery/todo-list/assets/js/todos.js
+++ bootcamp/section16-19-jquery/todo-list/assets/js/todos.js
@@ -1,5 +1,7 @@
+/* eslint-env jquery */
+
 // Check off specific to-dos by clicking
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
   // if li is gray, then turn it black
   if ($(this).css('color') === 'rgb(128, 128, 128)') {
     $(this).css({
@@ -12,4 +14,25 @@ $('li').click(function () {
       textDecoration: 'line-through'
     })
   }
+})
+
+$('ul').on('click', 'span', function (event) {
+  $(this).parent().fadeOut(500, function () {
+    $(this).remove()
+  })
+  event.stopPropagation()
+})
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    // grabbing new todo text from input
+    var todoText = $(this).val()
+    $(this).val('')
+    // create a new li and add to ul
+    $('ul').append('<li><span><i class="far fa-trash-alt"></i></span> ' + todoText + '</li>')
+  }
+})
+
+$('.fa-plus').click(function () {
+  $("input[type='text']").fadeToggle()
 })
