855d9396ffeb18ddfb6fc4fcfea7e2889cc6ca31
Ryan <rchillard@gmail.com>
2018-11-10 22:08:28 -0600

    Add final part of tutorial, Gatsby part 8

--- gatsby/tutorial-part-four/gatsby-config.js
+++ gatsby/tutorial-part-four/gatsby-config.js
@@ -18,5 +18,19 @@ module.exports = {
           pathToConfigModule: `src/utils/typography`,
         },
       },
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: "GatsbyJS",
+          short_name: "GatsbyJS",
+          start_url: "/",
+          background_color: "#6b37bf",
+          theme_color: "#6b37bf",
+          display: "minimal-ui",
+          icon: "src/images/icon.png", // This path is relative to the root of the site.
+        },
+      },
+      'gatsby-plugin-offline',
+      `gatsby-plugin-react-helmet`,
     ],
   }
