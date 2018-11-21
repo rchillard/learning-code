35e7d9c8d33cc394d233562fd3b405078434bcaa
RCH <rchillard@gmail.com>
2018-11-20 23:52:48 -0600

    Test out Prettier formatting on JSX

--- react/autocomplete-txt-box/src/App.js
+++ react/autocomplete-txt-box/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HideableText from './HideableText'
-import AutoCompleteText from './AutoCompleteText';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HideableText from "./HideableText";
+import AutoCompleteText from "./AutoCompleteText";
 
 class App extends Component {
   render() {
--- react/autocomplete-txt-box/src/HideableText.js
+++ react/autocomplete-txt-box/src/HideableText.js
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default class HideableText extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            isHidden: false,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHidden: false
+    };
+  }
 
-    toggleIsHidden () {
-        this.setState((currentState) => ({
-            isHidden: !currentState.isHidden,
-        }))
-    }
+  toggleIsHidden() {
+    this.setState(currentState => ({
+      isHidden: !currentState.isHidden
+    }));
+  }
 
-    render() {
-        return (
-            <div>
-                <button onClick={ () => this.toggleIsHidden() }>Toggle</button>
-                <div>
-                    {!this.state.isHidden && this.props.text }
-                </div>
-            </div>
-        )   
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.toggleIsHidden()}>Toggle</button>
+        <div>{!this.state.isHidden && this.props.text}</div>
+      </div>
+    );
+  }
 }
