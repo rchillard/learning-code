ca492fb63f2584ea38dc7effda2187f954e665d3
RCH <rchillard@gmail.com>
2018-12-29 14:23:39 -0600

    Save List.js from SimpleTodos

--- bootcamp-advanced/section26-create-react-app/simpletodos/src/List.js
+++ bootcamp-advanced/section26-create-react-app/simpletodos/src/List.js
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 import Todo from "./Todo.js";
 
 class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: ["Walk fish", "Trim sea cabbage", "Replace bubbler"]
-    };
-  }
+
   render() {
     const items = this.state.items.map((item, index) => (
       <Todo key={index} text={item} />
