dc362cf1dc453bb0e4dd01770b3907015dcbe47a
Ryan <rchillard@gmail.com>
2019-04-24 00:35:41 -0500

    Add Svelte exercises 18-21

---
+++ svelte/20-event-modifiers.svelte
@@ -0,0 +1,15 @@
+<script>
+	function handleClick() {
+		alert('clicked')
+	}
+</script>
+
+<button on:click|once={handleClick}>
+	Click me
+</button>
+
+preventDefault — calls event.preventDefault() before running the handler. Useful for e.g. client-side form handling
+stopPropagation — calls event.stopPropagation(), preventing the event reaching the next element
+passive — improves scrolling performance on touch/wheel events (Svelte will add it automatically where it's safe to do so)
+capture — fires the handler during the capture phase instead of the bubbling phase ()
+once — remove the handler after the first time it runs
