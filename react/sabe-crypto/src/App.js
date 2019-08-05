614e0b22b6f1bf3277bc766c933ce55a5285ee0a
RCH <rchillard@gmail.com>
2019-08-04 23:09:09 -0500

    Add Sabe Cryptocurrency Tickers exercise for React

---
+++ react/sabe-crypto/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Tickers from "./Tickers";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Cryptocurrency Ticker</h2>
+      </div>
+      <Tickers />
+    </div>
+  );
+}
+
+export default App;
