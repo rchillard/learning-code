6fae9ef9ff3b9dd2876d66eee1f9a7529fe2a3d2
RCH <rchillard@gmail.com>
2020-03-27 01:25:35 -0500

    Add basic serverless web app using s3-lambda-dynamodb

---
+++ serverless/s3-lambda-dynamo-web-app/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-2_Kyyjo8dOu", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7ojck5lc9fhbv7n8h2kipegvu", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2" // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://e501807xzl.execute-api.us-east-2.amazonaws.com/prod" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+  }
+};
