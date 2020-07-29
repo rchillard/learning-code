9752659e3956ac273c397a707b2d15227e3d3687
RCH <rchillard@gmail.com>
2020-07-29 00:22:44 -0500

    Add additional ownership exercises to Rust

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -14,4 +14,19 @@ fn main() {
     s2.push_str(" planet");
     println!("s2: {}", s2);
     println!("s1: {}", s1);
+
+    let sum: i32 = add_numbers(5, 10);
+    println!("{}", sum);
+
+    let ref_sum: i32 = 25;
+    println!("{}", add_ref_numbers(50, 100, &ref_sum));
+}
+
+fn add_numbers(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn add_ref_numbers(x: i32, y: i32, s: &i32) -> i32 {
+    println!("{}", s);
+    x + y
 }
