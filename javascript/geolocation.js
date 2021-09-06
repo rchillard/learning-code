be23c7b1f30b115c95b43803509e68bdb07a2fe2
RCH <rchillard@gmail.com>
2021-09-05 23:37:35 -0500

    Add watchPosition() to geolocation script example

--- javascript/geolocation.js
+++ javascript/geolocation.js
@@ -14,3 +14,14 @@ function error(error) {
 // Request access to location
 // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
 navigator.geolocation.getCurrentPosition(success, error);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/PositionOptions
+options = {
+    enableHighAccuracy: true, // requests best possible results (may consume battery)
+    timeout: 10000,           // maximum amount of time (in ms) device may take to return a position (default is infinity)
+    maximumAge: 30000         // maximum amount of time (in ms) of a possible cached position that is acceptable to return
+}
+
+// Request access to watch the user's location
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/watchPosition
+navigator.geolocation.watchPosition(success, error, options);
