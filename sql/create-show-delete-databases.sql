ad3abf88434fa3ccae5ea5e50de5614c03cf6d64
Ryan Hillard <20631917+rchillard@users.noreply.github.com>
2022-11-23 10:10:40 -0600

    Add comments to basic SQL

--- sql/create-show-delete-databases.sql
+++ sql/create-show-delete-databases.sql
@@ -1,9 +1,14 @@
+-- Create a new database
 CREATE DATABASE database_name;
 
+-- List all databases
 SHOW DATABASES;
 
+-- Use an individual database
 USE database_name;
 
+-- Display which database we are using
 SELECT database();
 
+-- Delete an entire database
 DROP DATABASE database_name;
--- sql/create-show-delete-tables.sql
+++ sql/create-show-delete-tables.sql
@@ -1,13 +1,17 @@
+-- Create a new table
 CREATE TABLE tablename
 	(
 		column_name datatype,
         column_name datatype
 	);
-    
+
+-- List all the tables in a database
 SHOW TABLES;
 
+-- Three different ways to see the contents of a table
 SHOW COLUMNS FROM tablename;
 DESCRIBE tablename;
 DESC tablename;
 
+-- Delete a table
 DROP TABLE tablename;
