d24cc118a82e2a0f8a9bc889c1f536b01bb41875
Ryan Hillard <20631917+rchillard@users.noreply.github.com>
2023-02-05 21:37:39 -0600

    Update create SQL script with additional options

--- sql/create-show-delete-tables.sql
+++ sql/create-show-delete-tables.sql
@@ -5,13 +5,51 @@ CREATE TABLE tablename
         column_name datatype
 	);
 
+-- Create a new table with NOT NULL
+CREATE TABLE tablename
+	(
+		column_name datatype NOT NULL,
+        column_name datatype NOT NULL
+	);
+
+-- Create a new table with DEFAULT
+CREATE TABLE tablename
+	(
+		column_name datatype DEFAULT default_value,
+        column_name datatype DEFAULT default_value
+	);
+
+-- Create a new table with NOT NULL and DEFAULT
+CREATE TABLE tablename
+	(
+		column_name datatype NOT NULL DEFAULT default_value,
+        column_name datatype NOT NULL DEFAULT default_value
+	);
+
+-- Create a new table with a PRIMARY KEY
+CREATE TABLE tablename
+	(
+		column_name datatype PRIMARY KEY,
+		column_name datatype
+	);
+
+-- Create a new table with an AUTO_INCREMENT column set to PRIMARY KEY
+CREATE TABLE tablename
+	(
+		column_name datatype AUTO_INCREMENT,
+		PRIMARY KEY(column_name)
+	);
+
+
 -- List all the tables in a database
 SHOW TABLES;
+
 
 -- Three different ways to see the columns of a table
 SHOW COLUMNS FROM tablename;
 DESCRIBE tablename;
 DESC tablename;
+
 
 -- Delete a table
 DROP TABLE tablename;
