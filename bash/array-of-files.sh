7333147f9e70081522806f4a7c76e08346103f15
RCH <20631917+rchillard@users.noreply.github.com>
2025-01-04 12:25:32 -0600

    Update array of files to include mapfile alternative

--- bash/array-of-files.sh
+++ bash/array-of-files.sh
@@ -11,3 +11,11 @@ IFS=$'\n' read -r -d '' -a html_files_array <<< "$html_files_string"
 for html_file in "${html_files_array[@]}"; do
   echo $html_file
 done
+
+# Alternative using mapfile (higher construct / program)
+# Create an array containing all .html files
+mapfile -t html_files_array < <(find ./build -maxdepth 1 -type f -name "*.html")
+
+for html_file in "${html_files_array[@]}"; do
+  echo $html_file
+done
